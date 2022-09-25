5783c75985e9293ae541288eed7d2f83f3783ba4
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-25 19:16:11 +0100

    test

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,12 @@
+const $ = window.$;
 $(document).ready(function () {
-let dict = {};
-var ckbox =$('input[type=checkbox]');
-
-$('input').click(function () {
-	if(ckbox.is(':checked')) {
-		dict[$(ckbox).data('id')] = $(ckbox).data('name');
-			$('.amenities h4').text(Object.values(dict).join(', '));
-	} else  {
-		delete dict[$(ckbox).data('id')];
-	$('.amenities h4').text(Object.values(dict).join(', '));
-	}
-});
+  const ckbox= {};
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).prop('checked') === true) {
+      ckbox [$(this).data('id')] = $(this).data('name');
+    } else if ($(this).prop('checked') === false) {
+      delete ckbox [$(this).attr('data-id')];
+    }
+    $('div.amenities H4').text(Object.values(ckbox).join(', '));
+  });
 });
